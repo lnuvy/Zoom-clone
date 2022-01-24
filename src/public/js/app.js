4ca5ffa4ab571af26827358e1d8f3a16eeff8bb3
lnuvy <hangeulgod@naver.com>
2022-01-24 14:52:14 +0900

    카메라 장치변경 bug 개선, 현재 선택된 장치가 option에 보이게 개선

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -99,13 +99,23 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -116,17 +126,21 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (e) {
     console.log(e);
   }
 }
-getCameras();
+// getCameras();
 
 function handleMuteClick() {
   myStream
@@ -154,8 +168,8 @@ function handleCameraClick() {
   }
 }
 
-function handleCameraChange() {
-  console.log(camerasSelect.value);
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
